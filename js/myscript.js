06050ae5d4ca7710a39db813f49267751e5bf77d
RUAN DOS SANTOS <100040124+Ruandossantos@users.noreply.github.com>
2023-05-13 19:40:19 -0300

    atualiza

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,21 @@
 const chaveapi = "8872410b5b1aedf65bcd58f2a7439315"
 
+function mostarnatela(dados){
+    console.log(dados)
+    document.querySelector(".nome_cidade_tempo").innerHTML="O tempo em " + dados.name
+    document.querySelector(".graus_city").innerHTML="Fazendo " + dados.main.temp
+    document.querySelector(".nubla_umidade").innerHTML="No nomento " + dados.weather[0].description
+    document.querySelector(".img_nuvem").src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+async function buscarcidade(cidade) {
+   const dados = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveapi}&lang={pt_br}&units=metric`).then(resposta => resposta.json())
+    mostarnatela(dados)
+}
+
+
 function funciaoclique() {
     let cidade = document.querySelector(".nome_cidade").value
     buscarcidade(cidade)
-}
-
-function buscarcidade(cidade) {
-    const dados = await fetch (`api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveapi}`).then( resosta => resposta.jason())
-
-    console.log(dados)
 }
