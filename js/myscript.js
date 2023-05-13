e0acaef1949545ebdc6f4c814bff684112b24a08
RUAN DOS SANTOS <100040124+Ruandossantos@users.noreply.github.com>
2023-05-13 16:13:33 -0300

    att

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,12 @@
+const chaveapi = "8872410b5b1aedf65bcd58f2a7439315"
 
-
-
-
-
-
-
-
-function funciaoclique (){
+function funciaoclique() {
     let cidade = document.querySelector(".nome_cidade").value
-    console.log(cidade)
+    buscarcidade(cidade)
+}
+
+function buscarcidade(cidade) {
+    const dados = await fetch (`api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveapi}`).then( resosta => resposta.jason())
+
+    console.log(dados)
 }
